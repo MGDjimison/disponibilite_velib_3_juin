78954f16ca088e7497d28b0c9f050e931ec80eac
djimi <djimi9530@gmail.com>
2025-10-23 14:05:33 +0200

    update/cleaning code

--- transform.py
+++ transform.py
@@ -37,72 +37,6 @@ def get_transformed_data():
     return df
 
 
-def get_numerical_columns():
-    # except id and code_postal
-    transformed_velib_df = get_transformed_data()
-    # select column which dtype is "int64"
-    df = transformed_velib_df.select_dtypes(include="int64")
-    # drop unwanted column
-    final_df = df.drop(["identifiant_station", "code_postal"], axis=1)
-    return final_df
-
-
-def get_average_of_columns_by_department():
-    transformed_velib_df = get_transformed_data()
-    transformed_velib_df["departement"] = add_department()
-    # get numerical columns dataframe
-    numerical_columns_df = get_numerical_columns()
-    # get names of numerical columns
-    numerical_columns = numerical_columns_df.columns
-
-    numerical_columns_df["departement"] = transformed_velib_df["departement"]
-    # print(numerical_columns_df)
-    data_groupby_department_df = numerical_columns_df.groupby("departement")[
-        numerical_columns
-    ].mean()
-    # round all columns
-    data_groupby_department_df = data_groupby_department_df.round()
-
-    return data_groupby_department_df
-
-
-def get_average_of_columns_by_city():
-    transformed_velib_df = get_transformed_data()
-    # get numerical columns dataframe
-    numerical_columns_df = get_numerical_columns()
-    # get names of numerical columns
-    numerical_columns = numerical_columns_df.columns
-
-    numerical_columns_df["commune"] = transformed_velib_df["commune"]
-    # print(numerical_columns_df)
-    data_groupby_city_df = numerical_columns_df.groupby("commune")[
-        numerical_columns
-    ].mean()
-    # round all columns
-    data_groupby_city_df = data_groupby_city_df.round()
-
-    return data_groupby_city_df
-
-
-def get_velib_in_paris():
-    df = get_transformed_data()
-    paris_velib_df = df[(df["commune"] == "Paris") & (df["station_en_fonctionnement"])]
-    return paris_velib_df
-
-
-def get_station_by_city():
-    df = get_transformed_data()
-    station_by_commune_df = df["commune"].value_counts()
-    return station_by_commune_df
-
-
-def get_station_by_department():
-    df = get_transformed_data()
-    df["departement"] = add_department()
-    station_by_departement_df = df["departement"].value_counts()
-    return station_by_departement_df
-
-
 def add_department():
     df = get_transformed_data()
     # convert "code_postal" to string and extract two first characters
@@ -122,28 +56,8 @@ def add_department():
     return df["departement"]
 
 
-def get_boolean_columns():
-    transformed_df = get_transformed_data()
-    # select boolean columns
-    df = transformed_df.select_dtypes(include="bool")
-    return df
-
-
-def get_count_on_boolean_column():
-    df = get_boolean_columns()
-    columns = df.columns
-
-    final_df = {}
-    for col in columns:
-        # create item with column name and count values
-        final_df[col] = df[col].value_counts()
-
-    return pandas.DataFrame(data=final_df)
-
-
-def get_address(coordinate):
+def get_address(coordinate: str):
     geolocator = Nominatim(user_agent="velib")
-    # reverse = RateLimiter(geolocator.reverse, min_delay_seconds=1)
     location = geolocator.reverse(coordinate)
     address = location.address.split(",")
     address = address[:3]

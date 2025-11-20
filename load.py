f286a4e926c1199a4ae55c0d588d66670ec20cbf
djimi <djimi9530@gmail.com>
2025-11-20 17:14:37 +0100

    update code 4

--- load.py
+++ load.py
@@ -2,19 +2,6 @@ import sqlite3
 import pandas as pd
 
 
-def get_connection():
-    conn = sqlite3.connect("data/disponibilite_velib.db")
-
-    return conn
-
-
-def get_cursor():
-    connection = get_connection()
-    cursor = connection.cursor()
-
-    return cursor
-
-
 def create_db_table(df: pd.DataFrame, table_name: str):
-    con = get_connection()
-    df.to_sql(name=table_name, con=con, if_exists="replace")
+    connection = sqlite3.connect("data/disponibilite_velib.db")
+    df.to_sql(name=table_name, con=connection, if_exists="replace")

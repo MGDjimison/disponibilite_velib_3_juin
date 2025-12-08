eb46b6041a4574cb8b46e476dbee2d55c478366c
djimi <djimi9530@gmail.com>
2025-12-08 10:34:14 +0100

    remove transform and load file

--- load.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import duckdb
-
-
-def load_data(df: pd.DataFrame):
-    """Create duckdb db and table with given dataframe"""
-    con = duckdb.connect("data/disponibilite_velib.db")
-    con.sql("CREATE TABLE velib AS SELECT * FROM df")
